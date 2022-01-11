0ea09b7c0d289d86904f5f9539732da4bac1e747
slqy <44186897+slqy@users.noreply.github.com>
2022-01-11 17:46:29 +0800

    Add files via upload

---
+++ Qn2c.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Qn2c {
+
+    public static void main(String[] args) {
+        // write your code here
+        for (int x = 101; x > 65; x = x-2)
+        {
+            System.out.println("Value of x: " + x);
+            System.out.println("\n");
+        }
+    }
+}
