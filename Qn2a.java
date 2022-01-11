42ff2242a2cd542ba17b4e96863f090d0e74299a
slqy <44186897+slqy@users.noreply.github.com>
2022-01-11 18:09:00 +0800

    Add files via upload

---
+++ Qn2a.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Qn2a {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, I am Samantha");
+    }
+}
---
+++ Qn2b.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Qn2b {
+
+    public static void main(String[] args) {
+        // write your code here
+        String module = "CSC1009";
+        switch (module)
+        {
+            case "CSC1006":
+                System.out.println("Mathematics 2");
+                break;
+            case "CSC1007":
+                System.out.println("Operating Systems");
+                break;
+            case "CSC1008":
+                System.out.println("Data Structures and Algorithms");
+                break;
+            case "CSC1009":
+                System.out.println("Object-Oriented Programming");
+                break;
+            case "CSC1010":
+                System.out.println("Computer Networks");
+                break;
+        }
+    }
+}
