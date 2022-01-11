c8d03a7ccaa8ea87cf13a60ab63fd50b7d159ad7
slqy <44186897+slqy@users.noreply.github.com>
2022-01-11 18:07:52 +0800

    Delete Qn2a.java

--- Qn2a.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Qn2a {
-
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello, I am Samantha");
-    }
-}
